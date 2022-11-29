c62c27ffa5577c6ebe30bc1270b6b3fefe3d46ba
Henrique Alves <henriqueadlima@hotmail.com>
2022-11-29 20:59:58 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import reactLogo from './assets/react.svg'
+import AppRoutes from '../routes'
+
+import Navbar from './components/Navbar'
+import "./styles/global.styles.scss";
+
+function App() {
+  return (
+    <>
+        <AppRoutes/>
+    </>
+
+  )
+}
+
+export default App
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss'
+
+function Navbar(){
+    return(
+        <nav className={styles.nav}>
+            <a href="">Home</a>
+        </nav>
+    )
+}
+
+export default Navbar
