3b024c3fdba5e75db6af80a33023b47ad3ef3fcf
Henrique Alves <henriqueadlima@hotmail.com>
2022-11-29 21:27:18 -0300

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App'
+import App from "./App";
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
-
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <App />
+    </BrowserRouter>
+);
